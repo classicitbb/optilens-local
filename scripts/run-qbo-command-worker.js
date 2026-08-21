1924a5305581fb48da2e7b8756cca13ecbb180a3
classicitbb <classic.it@outlook.com>
2026-08-21 08:13:11 -0400

    Revert "Add outbound QBO command worker"
    This reverts commit b865d115b7a711646d1820e1f25ae6e0172b7304.

--- scripts/run-qbo-command-worker.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-const { runOnce } = require('../lib/qbo-command-worker');
-runOnce().then((result) => console.log(JSON.stringify(result))).catch((error) => { console.error(error.message); process.exitCode = 1; });
